a31cfb1a65b8ca09c1d14b3c1162f289a27cc357
fgalliat <fgalliat@gmail.com>
2020-02-29 22:30:38 +0100

    REFACTORED ConsoleAPI + ADDED tft Support

--- tests/serialPutty/GenericConsole.h
+++ tests/serialPutty/GenericConsole.h
@@ -5,6 +5,11 @@
  * Xtase - fgalliat @Feb2020
  */
 
+#include "SerialTermConsole.h"
+#include "SerialDummyConsole.h"
+#include "TFTConsole.h"
+
+
 #define CONSOLE_MODE_SERIAL_DUMMY 1
 #define CONSOLE_MODE_SERIAL_VT100 2
 #define CONSOLE_MODE_TFT          4
@@ -27,20 +32,87 @@ class GenericConsole : public Print {
     GenericConsole(uint8_t mode) : Print() {
         this->mode = mode;
         this->serialVtMode = true;
-    }   
+    }
 
-    size_t write(uint8_t character) {
+    void init() {
         if ( this->hasSerial() ) {
             if ( this->isSerialDummy() ) {
-                return con_dum()->write(character);
+                con_dum_init();
             } else {
-                return con_ser()->write(character);
+                con_ser_init();
+            }
+        }
+        
+        if ( this->hasScreen() ) {
+            con_tft_init();
+        }
+    }
+
+    void cls() {
+        if ( this->hasSerial() ) {
+            if ( this->isSerialDummy() ) {
+                con_dum_cls();
+            } else {
+                con_ser_cls();
             }
         }
         if ( this->hasScreen() ) {
-            con_dum()->write('!');
-            return con_dum()->write(character);
+            con_tft_cls();
         }
-        return con_dum()->write(character);
+    }
+
+    void cursor(int row, int col) {
+        if ( this->hasSerial() ) {
+            if ( this->isSerialDummy() ) {
+                con_dum_cursor(row, col);
+            } else {
+                con_ser_cursor(row, col);
+            }
+        }
+        if ( this->hasScreen() ) {
+            con_tft_cursor(row, col);
+        }
+    }
+
+    void attr_accent() {
+        if ( this->hasSerial() ) {
+            if ( this->isSerialDummy() ) {
+                con_dum_attr_accent();
+            } else {
+                con_ser_attr_accent();
+            }
+        }
+        if ( this->hasScreen() ) {
+            con_tft_attr_accent();
+        }
+    }
+
+    void attr_none() {
+        if ( this->hasSerial() ) {
+            if ( this->isSerialDummy() ) {
+                con_dum_attr_none();
+            } else {
+                con_ser_attr_none();
+            }
+        }
+        if ( this->hasScreen() ) {
+            con_tft_attr_none();
+        }
+    }
+
+    // inheritance
+    size_t write(uint8_t character) {
+        size_t res = 0;
+        if ( this->hasSerial() ) {
+            if ( this->isSerialDummy() ) {
+                res = con_dum()->write(character);
+            } else {
+                res = con_ser()->write(character);
+            }
+        }
+        if ( this->hasScreen() ) {
+            res = con_tft()->write(character);
+        }
+        return res;
     }
 };
--- tests/serialPutty/serialPutty.ino
+++ tests/serialPutty/serialPutty.ino
@@ -2,73 +2,70 @@
  * Putty VT-xx escapes tests 
  */
 
-#include "SerialTermConsole.h"
-#include "SerialDummyConsole.h"
 #include "OutputConsole.h"
 
 #include "GenericConsole.h"
-GenericConsole console(CONSOLE_MODE_SERIAL_VT100);
+GenericConsole console(CONSOLE_MODE_SERIAL_VT100 | CONSOLE_MODE_TFT );
 
 
 void setup() {
-    con_ser_init();
+    // con_ser_init();
+    console.init();
 }
 
 
 void loop() {
     if ( Serial.available() > 0 ) {
         int ch = Serial.read();
-        Serial.print( "ch : " );
-        Serial.print( ch );
-        Serial.print( " " );
-        Serial.println( (ch == 27 ? '^' : (char)ch) );
+        console.print( "ch : " );
+        console.print( ch );
+        console.print( " " );
+        console.println( (ch == 27 ? '^' : (char)ch) );
 
 
         if ( ch == 9 ) {
             // Tab
             //http://manpagesfr.free.fr/man/man4/console_codes.4.html
-
             // http://www.termsys.demon.co.uk/vtansi.htm
 
-            con_ser_cls();
+            console.cls();
 
-            con_ser_cursor(1, 1);
-            Serial.print( splash_screen );
-            con_ser_cursor(10, 40);
-            con_ser_attr_accent();
-            Serial.print("Missing BOOT-DISK /!\\");
-            con_ser_attr_none();
-            Serial.println();
+            console.cursor(1, 1);
+            console.print( splash_screen );
+            console.cursor(10, 40);
+            console.attr_accent();
+            console.print("Missing BOOT-DISK /!\\");
+            console.attr_none();
+            console.println();
 
 
-            Serial.write("coucou");
+            console.print("coucou");
 
             // row, col 1-Based
-            con_ser_cursor(3, 10);
+            console.cursor(3, 10);
             
 
-            con_ser_attr_accent();
-            Serial.print("TOTO");
-            con_ser_attr_none();
+            console.attr_accent();
+            console.print("TOTO");
+            console.attr_none();
 
-            Serial.println("");
+            console.println("");
 
 
-            con_ser_cursor(1, 1);
+            console.cursor(1, 1);
             for(int x=0; x < con_ser_width(); x++) {
-                Serial.print( (x%10) );
+                console.print( (x%10) );
             }
             for(int y=0; y < con_ser_height(); y++) {
-                con_ser_cursor(y+1, 1);
-                Serial.print( y );
+                console.cursor(y+1, 1);
+                console.print( y );
             }
 
-            con_ser_cursor(15, 45);
-            con_ser()->println( 12.125 );
+            console.cursor(15, 45);
+            console.println( 12.125 );
 
-            con_ser_cursor(17, 45);
+            console.cursor(17, 45);
             console.println("Write from ganaric class");
-
 
         }
 
