ce9e7ab5fd08da2bd7f2e2fc9230acd9bc3138d5
Franck GALLIAT <fgalliat@gmail.com>
2020-03-02 21:23:47 +0100

    fix(tft): console scroll

--- tests/console/TFTConsole.h
+++ tests/console/TFTConsole.h
@@ -119,29 +119,14 @@ void _scrollTop(int howMany=1) {
   int mLen = ( (TFT_CAP_HEIGHT - howMany)*TFT_CAP_WIDTH);
   int mAddr = (howMany*TFT_CAP_WIDTH);
   int mlen = ( (howMany)*TFT_CAP_WIDTH);
-/* */
+
   memmove( &ttyMEMSEG[0], &ttyMEMSEG[ mAddr ], mLen );
   memmove( &ttyAttrMEMSEG[0], &ttyAttrMEMSEG[ mAddr ], mLen );
- /* */
-
-
-
-  // char tmpMEMSEG[TFT_CAP_WIDTH*TFT_CAP_HEIGHT];
-  // memset( &tmpMEMSEG[0], 0x00, mLen );
-  // memcpy( &tmpMEMSEG[0], &ttyMEMSEG[ mAddr ], mLen );
-  // memset( &ttyMEMSEG[0], 0x00, mLen );
-  // memcpy( &ttyMEMSEG[0], &tmpMEMSEG[0], mLen );
-
-  // memset( &tmpMEMSEG[0], 0x00, mLen );
-  // memcpy( &tmpMEMSEG[0], &ttyAttrMEMSEG[ mAddr ], mLen );
-  // memset( &ttyAttrMEMSEG[0], 0x00, mLen );
-  // memcpy( &ttyAttrMEMSEG[0], &tmpMEMSEG[0], mLen );
-
   // blank mem
   memset( &ttyMEMSEG[mAddr], 0x00, mlen );
   memset( &ttyAttrMEMSEG[mAddr], 0x00, mlen );
 
-tft.fillRect(0, 0, 480, (TFT_CAP_HEIGHT-howMany)*8, CLR_TXT_BCK);
+  tft.fillRect(0, 0, 480, (TFT_CAP_HEIGHT-howMany)*8, CLR_TXT_BCK);
   _redrawWholeFrame();
 
   con_tft_cursor( TFT_CAP_HEIGHT, 1 );
@@ -182,22 +167,17 @@ void con_tft_writeOneChar(char ch) {
 }
 
 void _redrawWholeFrame() {
-  // tft.fillRect(0, 480, CLR_TXT_BCK);
   int mlen = TFT_CAP_WIDTH * TFT_CAP_HEIGHT;
   char ch; int attr; uint16_t fg,bg;
   int x = 0, y = 0;
   for(int i=0; i < mlen; i++) {
     ch = ttyMEMSEG[i];
-    // ch = ch != 0x00 ? 'x' : 0x00;
     if ( !( ch == 0x00 || ch == 0x0A || ch == 0x0D )) { 
       attr = (int)ttyAttrMEMSEG[i];
       fg = tty_colors[ attr ];
       bg = tty_bg_colors[ attr ];
       tft.drawChar(x, y, ch, fg, bg, 1);
     } 
-    // else {
-    //   tft.drawChar(x, y, ' ', CLR_TXT_DEF, CLR_TXT_BCK, 1);
-    // }
 
     x += 6; if ( x >= 480 ) { x = 0; y += 8; }
   }
