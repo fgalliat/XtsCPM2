06e31dbd3ef3d609189a5e8a4ed6d915badff0d5
Franck GALLIAT <fgalliat@gmail.com>
2020-03-04 08:40:22 +0100

    fix(wifi): fix in progress for readLine()

--- tests/wifi/wifi.ino
+++ tests/wifi/wifi.ino
@@ -78,7 +78,9 @@ void loop() {
 
     // Serial.println( "loop" );
 
-    char* ignored = wifi_wget((char*)"$home", 8666, "/sensors/sensor/1", (char*)HEADERS);
+    char* api = (char*)"'http://192.168.1.134:8666/sensors/sensor/1'";
+
+    char* ignored = wifi_wget((char*)"$home", 8666, api, (char*)HEADERS);
     Serial.println( ignored );
 
 Serial.println( "-- EOF --" );
