8f42dbda231e0cdc2e4a132be9d6a2022eac9d42
fgalliat <fgalliat@gmail.com>
2020-04-16 19:49:30 +0200

    feat(jni80): start working on JNICallback that returns a value (wip)

--- src/java/jni80/JavaRunCPM.cpp
+++ src/java/jni80/JavaRunCPM.cpp
@@ -4,8 +4,11 @@
 
 // Globals
 static jmethodID midStr;
-//static char * sigStr = "(Ljava/lang/String;ILjava/lang/String;)V";
-static const char * sigStr = "(II)V";
+
+static bool xtsBdosCallMeth_inited = false;
+
+// for a Java .xxx(String, int, String) -> void
+//static const char * sigStr = "(Ljava/lang/String;ILjava/lang/String;)V";
 
 
 
@@ -21,33 +24,39 @@ static const char * sigStr = "(II)V";
 // }
 
 // Methods
-static void javaDefineString(JNIEnv * env, jobject o,  jint reg, jint value) {
+static jint XtsBdosCall(JNIEnv * env, jobject o,  jint reg, jint value) {
 
-  jclass _class = (env)->GetObjectClass( o );
-  // Init - One time to initialize the method id, (use an init() function)
-  // midStr = (env)->GetMethodID( _class, "test_XtsBdosCall", sigStr);
+  if ( xtsBdosCallMeth_inited == false ) {
+    printf("looking for Java method ...\n");
 
-  // public int XtsBdosCall(int reg, int value)
-  midStr = (env)->GetMethodID( _class, "XtsBdosCall", "(II)I");
+    jclass _class = (env)->GetObjectClass( o );
+    // Init - One time to initialize the method id, (use an init() function)
+    // midStr = (env)->GetMethodID( _class, "test_XtsBdosCall", sigStr);
+
+    // public int XtsBdosCall(int reg, int value)
+    midStr = (env)->GetMethodID( _class, "XtsBdosCall", "(II)I");
+
+    xtsBdosCallMeth_inited = true;
+  }
 
   // jstring string = (*env)->NewStringUTF(env, name);
   // (env)->CallVoidMethod( o, midStr, reg, value);
   jint result = (env)->CallIntMethod( o, midStr, reg, value);
 
-  printf("result of call [%d]\n", result);
+  return result;
 }
 
 
 JNIEXPORT void JNICALL Java_JavaRunCPM_startCPM
   (JNIEnv * env, jobject _this) {
 
-printf("coucou\n");
-
     jint reg = 228;
     jint value = 6;
 
-    javaDefineString(env, _this,   reg, value);
+    jint result = XtsBdosCall(env, _this,   reg, value);
+    printf("result of call [%d]\n", result);
 
-printf("bye bye\n");
+    result = XtsBdosCall(env, _this,   225, 0x1F);
+    printf("result of call [%d]\n", result);
 
 }
