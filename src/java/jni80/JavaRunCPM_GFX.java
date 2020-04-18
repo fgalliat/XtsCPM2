da51c8112af394c28d26c3fc431bc762de4e57e2
fgalliat <fgalliat@gmail.com>
2020-04-18 15:33:06 +0200

    feat(jni80): add Gfx TTY (wip)

--- src/java/jni80/JavaRunCPM_GFX.java
+++ src/java/jni80/JavaRunCPM_GFX.java
@@ -31,6 +31,8 @@ public class JavaRunCPM_GFX extends JavaRunCPM {
         protected char[][] tty = new char[TTY_ROWS][TTY_COLS];
         protected int ttyCursorX = 0;
         protected int ttyCursorY = 0;
+        protected boolean ttyDirty = false;
+        protected boolean bufDirty = false;
 
 
         public Video() {
@@ -47,12 +49,87 @@ public class JavaRunCPM_GFX extends JavaRunCPM {
             g.setColor(Color.BLUE);
             g.setFont(monospaced);
 
-            g.drawString("= XtsJ80 =", 10, FONT_HEIGHT+10);
+            // g.drawString("= XtsJ80 =", 10, FONT_HEIGHT+10);
+
+
+            // TODO : use a dblBuff
+
+            g.setColor( Color.BLACK );
+            g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+            if ( true || ttyDirty ) {
+                g.setColor( Color.BLUE );
+                for(int y=0; y < TTY_ROWS; y++) {
+                    // for(int x=0; x < TTY_COLS; x++) {
+                    //     char ch = tty[y][x];
+                    //     if ( ch != 0 ) {
+                    //         g.drawChars(data, offset, length, x, y);
+                    //     }
+                    // } 
+                    // FIXME : escapes char ....
+                    g.drawChars( tty[y] , 0, TTY_COLS, 0, FONT_HEIGHT+(y*FONT_HEIGHT));
+                }
+                ttyDirty = false;
+            }
+
+        }
+
+        public void cls() {
+            for(int y=0; y < TTY_ROWS; y++) {
+                for(int x=0; x < TTY_COLS; x++) {
+                    tty[y][x] = 0x00;
+                } 
+            }
+            // dblBuff.erase
+            ttyDirty = true;
+            // ttyDirty = false; // to speed up
+            bufDirty = true;
+        }
+
+        protected void _scrollUp() {
+            for(int i=1; i < TTY_ROWS; i++) {
+                tty[i-1] = tty[i];
+            }
+            tty[TTY_ROWS-1] = new char[ TTY_COLS ];
+            ttyDirty = true;
+        }
+
+        protected void _br() {
+            ttyCursorX = 0;
+            ttyCursorY++;
+            if ( ttyCursorY >= TTY_ROWS ) {
+                _scrollUp();
+            }
+            ttyDirty = true;
         }
 
         public void put_ch(char ch) {
 
+            if ( ch == '\r' ) {
+                _br();
+                return;
+            }
+
+            tty[ttyCursorY][ttyCursorX] = ch;
+            ttyCursorX++;
+            if ( ttyCursorX >= TTY_COLS ) {
+                _br();
+            }
+            ttyDirty = true;
         }
+
+        public void put_str(String str) {
+            for(int i=0; i < str.length(); i++) {
+                put_ch(str.charAt(i));
+            }
+            ttyDirty = true;
+        }
+
+        public void refresh() {
+            repaint();
+            ttyDirty = false;
+        }
+
 
     }
 
@@ -80,7 +157,7 @@ public class JavaRunCPM_GFX extends JavaRunCPM {
             public void run() {
                 boolean inRun = true;
                 while( inRun ) {
-                    vid.repaint();
+                    vid.refresh();
                     Zzz(100);
                 }
             }
@@ -88,6 +165,19 @@ public class JavaRunCPM_GFX extends JavaRunCPM {
 
 
     }
+
+
+    // ============================================
+
+    int  _ext_kbhit()        { return 0; }
+    char _ext_getch()        { return (char)0; }
+    void _ext_putch(char ch) { vid.put_ch(ch); }
+
+    void _ext_coninit()    { System.out.println("J> Init the console.\n"); }
+    void _ext_conrelease() { System.out.println("J> Release the console.\n"); }
+    void _ext_clrscr()     { vid.cls(); }
+
+    // ============================================
 
     public static void Zzz(long millis) {
         try { Thread.sleep(millis); }
@@ -97,6 +187,8 @@ public class JavaRunCPM_GFX extends JavaRunCPM {
 
     public JavaRunCPM_GFX() {
         initGUI();
+
+        vid.put_str("Hello World");
     }
 
 
