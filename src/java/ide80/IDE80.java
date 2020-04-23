625a51c77f48a014107d9602ec9cec3120cebdc4
fgalliat <fgalliat@gmail.com>
2020-04-23 17:52:34 +0200

    feat(ide80): start to dynamicaly link JavaPascalCompiler runtime

--- src/java/ide80/IDE80.java
+++ src/java/ide80/IDE80.java
@@ -2,6 +2,8 @@
 import java.awt.GridLayout;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +12,37 @@ import javax.swing.JScrollPane;
 public class IDE80 extends JFrame {
 
     public static void main(String[] args) {
+
+        try {
+            Class clazz = Class.forName("JavaPascalCompiler");
+
+            // needs a no-params constructor
+            Object instance = clazz.newInstance();
+
+            Method foundMeth = null;
+            Method[] meths = clazz.getDeclaredMethods();
+            for(Method meth : meths) {
+                if ( meth.getName().equals("compile") ) {
+                    foundMeth = meth;
+                    break;
+                }
+            }
+
+            System.out.println( foundMeth.getName() +" => "+ foundMeth.toString() );
+
+            foundMeth.invoke(instance, new Object[] { "c:bmp.pas" } );
+
+            System.out.println("JavaPascalCompiler is available ;-) ");
+        } catch(ClassNotFoundException ex) {
+            System.out.println("JavaPascalCompiler is not available :-( ");
+        } catch(InstantiationException | IllegalAccessException ex) {
+            System.out.println("JavaPascalCompiler is not instanciable :-( ");
+        } catch(InvocationTargetException ex) {
+            System.out.println("JavaPascalCompiler.compile() is not invokable :-( ");
+        }
+
+
+
         IDE80 xmlEditor = new IDE80();
         xmlEditor.setVisible(true);
     }
--- src/java/ide80/run.sh
+++ src/java/ide80/run.sh
@@ -1,1 +1,2 @@
-java -cp bin IDE80
+# java -cp bin IDE80
+java -cp bin:../jni80/bin IDE80
